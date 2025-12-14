4408fd9c4e33469f0497c0e1a6642419ef9f0e6e
GJ <newstart.infotech@gmail.com>
2025-12-14 20:13:11 +0100

    add script to install needed ubuntu packages

---
+++ setup-ubu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -xe
+sudo apt install universal-ctags silversearcher-ag snapd
+sudo snap install go --classic
+
+wget https://nodejs.org/dist/v24.12.0/node-v24.12.0-linux-x64.tar.xz
+sudo tar xvf node-v24.12.0-linux-x64.tar.xz -C /opt
+sudo ln -s /opt/node-v24.12.0-linux-x64/bin/* /usr/local/bin
+
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 \
+	-O /usr/local/bin/yq && sudo chmod +x /usr/local/bin/yq
