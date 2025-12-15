2dfec0d33cb2230a23fb4fe50c409cdf412c5cf8
GJ <newstart.infotech@gmail.com>
2025-12-15 06:59:17 +0100

    improve icmk8s.ch

--- icmk8s.sh
+++ icmk8s.sh
@@ -6,30 +6,31 @@
 set -xe
 ip=${1:-10.0.0.1}
 usr=$2
-snap install microk8s --classic --channel=latest/stable
-pushd /var/snap/microk8s/current > /dev/null
-cat >>args/kube-apiserver <<-EOF
+pwd=$(pwd)
+sudo snap install microk8s --classic --channel=latest/stable
+sudo sh -c 'cd /var/snap/microk8s/current/args > /dev/null'
+sudo cat >>kube-apiserver <<-EOF
 	# customize interface
 	--advertise-address=$ip
 	--bind-address=0.0.0.0
 	EOF
-cat >>args/kube-controller-manager <<-EOF
+sudo cat >>kube-controller-manager <<-EOF
 	# customize interface
 	--bind-address=0.0.0.0
 	EOF
-cat >>args/kube-scheduler <<-EOF
+sudo cat >>kube-scheduler <<-EOF
 	# customize interface
 	--bind-address=0.0.0.0
 	EOF
-cat >>args/kube-proxy <<-EOF
+sudo cat >>kube-proxy <<-EOF
 	# customize interface
 	--bind-address=0.0.0.0
 	EOF
-cat >>args/kubelet <<-EOF
+sudo cat >>kubelet <<-EOF
 	--address=0.0.0.0
 	--node-ip=$ip
 	--healthz-bind-address=127.0.0.1
 	EOF
-snap restart microk8s
-[ -n $usr ] && usermod -a -G microk8s $usr
-popd > /dev/null
+sudo snap restart microk8s
+[ -n $usr ] && sudo usermod -a -G microk8s $usr
+sudo sh -c 'cd $pwd > /dev/null'
