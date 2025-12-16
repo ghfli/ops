dac4204aa001eb0f4254a7a70fec52913c6cde2d
GJ <newstart.infotech@gmail.com>
2025-12-16 06:49:25 +0100

    fix errors in icmk8s.sh

--- icmk8s.sh
+++ icmk8s.sh
@@ -6,31 +6,29 @@
 set -xe
 ip=${1:-10.0.0.1}
 usr=$2
-pwd=$(pwd)
+mca=/var/snap/microk8s/current/args
 sudo snap install microk8s --classic --channel=latest/stable
-sudo sh -c 'cd /var/snap/microk8s/current/args > /dev/null'
-sudo cat >>kube-apiserver <<-EOF
+sudo sh -c "cat >>$mca/kube-apiserver <<-EOF
 	# customize interface
 	--advertise-address=$ip
 	--bind-address=0.0.0.0
-	EOF
-sudo cat >>kube-controller-manager <<-EOF
+	EOF"
+sudo sh -c "cat >>$mca/kube-controller-manager <<-EOF
 	# customize interface
 	--bind-address=0.0.0.0
-	EOF
-sudo cat >>kube-scheduler <<-EOF
+	EOF"
+sudo sh -c "cat >>$mca/kube-scheduler <<-EOF
 	# customize interface
 	--bind-address=0.0.0.0
-	EOF
-sudo cat >>kube-proxy <<-EOF
+	EOF"
+sudo sh -c "cat >>$mca/kube-proxy <<-EOF
 	# customize interface
 	--bind-address=0.0.0.0
-	EOF
-sudo cat >>kubelet <<-EOF
+	EOF"
+sudo sh -c "cat >>$mca/kubelet <<-EOF
 	--address=0.0.0.0
 	--node-ip=$ip
 	--healthz-bind-address=127.0.0.1
-	EOF
+	EOF"
 sudo snap restart microk8s
 [ -n $usr ] && sudo usermod -a -G microk8s $usr
-sudo sh -c 'cd $pwd > /dev/null'
